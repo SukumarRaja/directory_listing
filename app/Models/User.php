2035e84d120a6c25b8d892422a72641dc31df0cd
Arulkumaran C <arulkumaran.c@outlook.com>
2023-08-15 11:24:02 +0530

    added activity and query logging

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,10 +7,12 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Spatie\Activitylog\LogOptions;
+use Spatie\Activitylog\Traits\LogsActivity;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable, LogsActivity;
 
     /**
      * The attributes that are mass assignable.
@@ -42,4 +44,17 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    /**
+     * Get the options for the activity log.
+     *
+     * @return LogOptions
+     */
+    public function getActivitylogOptions(): LogOptions
+    {
+        return LogOptions::defaults()
+            ->logOnly(array_diff($this->fillable, $this->hidden))
+            ->logOnlyDirty()
+            ->dontSubmitEmptyLogs();
+    }
 }
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +12,26 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        if ($this->app->isLocal()) {
+
+            // Log queries in development
+            DB::listen(function ($query) {
+                $sql = $query->sql;
+                $bindings = $query->bindings;
+                $time = $query->time;
+
+                // Exclude specific tables from logging
+                $excludedTables = ['activity_log', 'telescope_entries', 'telescope_entries_tags', 'telescope_monitoring'];
+
+                foreach ($excludedTables as $table) {
+                    if (str_contains($sql, $table)) {
+                        return;
+                    }
+                }
+
+                logger("Query: $sql", ['bindings' => $bindings, 'time' => $time]);
+            });
+        }
     }
 
     /**
